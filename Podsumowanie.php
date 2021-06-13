6adbd29321b3fc35837f24941c05cd865f9a0322
RyyyyC <85810595+RyyyyC@users.noreply.github.com>
2021-06-13 18:13:09 +0200

    Add files via upload

---
+++ Podsumowanie.php
@@ -0,0 +1,7 @@
+<?php
+    $_SESSION['imie'] = $_POST['imie'];
+    $_SESSION['nazwisko'] = $_POST['nazwisko'];
+    $_SESSION['email'] = $_POST['email'];
+    $_SESSION['numerTlefonu'] = $_POST['numer-telefonu'];
+    $_SESSION['numerKierunkowy'] = $_POST['numer_kierunkowy'];
+?>
