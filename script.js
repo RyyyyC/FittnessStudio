ecd03fe9d8acdb7c381d3c2ac7a361d673b7d32a
RyyyyC <85810595+RyyyyC@users.noreply.github.com>
2021-06-13 12:08:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+h = window.innerHeight
+w = window.innerWidth
+
+
+/*function resizeAnimation1(){
+    console.log("Height: " + h)
+    console.log("Width: " + w)
+    console.log("Nowy scale " + (w * 0.8) * 0.4 + ' ' + (h * 0.8) * 0.4)
+    document.getElementsByClassName("Animacja1").style.transform = document.getElementsByClassName("Animacja1").style.transform.replace(
+        /scale\([0-9|\.]*\)/, 'scale(' + w * 0.4 + ' ' + h * 0.4 + ')'
+        )
+    
+}*/
